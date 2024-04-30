81b71f516c058c60f9e1f42a8550f1c841ceed9d
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-04-30 14:29:53 +0900

    three-5 // line

---
+++ basic05/js/script.js
@@ -0,0 +1,29 @@
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.164.0/three.module.min.js';
+import * as stats from 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/7/Stats.min.js';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const scene = new THREE.Scene();
+
+// 재질 정의
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff });
+
+// 꼭지점에 대한 기하학 정의
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+// 선 정의 -> 점과 재질을 합쳐서
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
